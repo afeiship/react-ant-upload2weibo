9de8b7787cb9b23110247e211ebef04c91b32578
afeiship <1290657123@qq.com>
2022-02-13 01:15:05 +0800

    feat: previewable done

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 import noop from '@jswork/noop';
 import classNames from 'classnames';
 import React, { Component } from 'react';
-import { Upload } from 'antd';
+import { Modal, Upload } from 'antd';
 import NxLeancloudOptions from '@afeiship/next-leancloud-options';
 import NxWeiboOss from '@jswork/next-weibo-oss';
 
@@ -16,6 +16,7 @@ export type ReactAntUpload2weiboProps = {
    * The change handler.
    */
   onChange?: Function;
+  previewProps?: any;
 };
 
 export default class ReactAntUpload2weibo extends Component<ReactAntUpload2weiboProps> {
@@ -26,6 +27,20 @@ export default class ReactAntUpload2weibo extends Component<ReactAntUpload2weibo
   };
 
   private ossClient: any = null;
+  private fileList: any = [];
+
+  state = {
+    previewVisible: false,
+    currentIndex: -1
+  };
+
+  get done() {
+    return this.fileList.every((item) => item.percent === 100);
+  }
+
+  get value() {
+    return this.fileList.map((item) => item.response);
+  }
 
   async componentDidMount() {
     const lcOpts = new NxLeancloudOptions({ id: '60f768f6d9f1465d3b1d5c43' });
@@ -33,37 +48,54 @@ export default class ReactAntUpload2weibo extends Component<ReactAntUpload2weibo
     this.ossClient = new NxWeiboOss(res.value);
   }
 
-  handleBeforeUpload = () => {
-    console.log('click me!');
-  };
-
   handleChange = (inEvent) => {
-    if(inEvent.percent === 100) {
-      console.log('change.!', inEvent);
+    const { onChange } = this.props;
+    this.fileList = inEvent.fileList;
+    if (this.done) {
+      onChange!({ target: { value: this.value } });
     }
   };
 
   handleUploadRequest = (inEvent) => {
-    this.ossClient.upload(inEvent.file).then((res) => {
-      console.log('res:', res[0].url);
-      inEvent.onSuccess(res[0].url);
+    const { file } = inEvent;
+    this.ossClient.upload(file).then((res) => {
+      inEvent.onSuccess(res[0].url, file);
     });
   };
 
+  handleModelCancel = () => {
+    this.setState({ previewVisible: false });
+  };
+
   render() {
-    const { className, onChange, ...props } = this.props;
+    const { className, onChange, previewProps, ...props } = this.props;
+    const { currentIndex, previewVisible } = this.state;
 
     return (
       <div data-component={CLASS_NAME} className={classNames(CLASS_NAME, className)}>
         <Upload
           listType="picture-card"
           multiple
-          beforeUpload={this.handleBeforeUpload}
           customRequest={this.handleUploadRequest}
           onChange={this.handleChange}
+          onPreview={(file) => {
+            const currentIndex = this.fileList.indexOf(file);
+            this.setState({ previewVisible: true, currentIndex });
+          }}
           {...props}>
           + 上传图片
         </Upload>
+        <Modal
+          visible={previewVisible}
+          title="图片预览"
+          width={800}
+          footer={null}
+          onCancel={this.handleModelCancel}
+          {...previewProps}>
+          {currentIndex >= 0 && (
+            <img alt="example" style={{ width: '100%' }} src={this.value[currentIndex]} />
+          )}
+        </Modal>
       </div>
     );
   }
